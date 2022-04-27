da95f683f2ea4bd575062870c6cff3712881bfd5
Jochen Hilgers <jochen.hilgers@code-intelligence.com>
2022-04-27 12:37:06 +0200

    docs(init): improve output messages

--- internal/cmd/init.go
+++ internal/cmd/init.go
@@ -28,19 +28,20 @@ func runInitCommand(cmd *cobra.Command, args []string) (err error) {
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	out.Debug("using current working directory: %s", cwd)
+	out.Debug("Using current working directory: %s", cwd)
 
 	configpath, err := config.CreateProjectConfig(cwd, fs)
 	if err != nil {
 		// explicitly inform the user about an existing config file
 		if os.IsExist(errors.Cause(err)) && configpath != "" {
-			out.Warn("config already exists in %s", configpath)
+			out.Warn("Config already exists in %s", configpath)
 			err = cmdutils.WrapSilentError(err)
 		}
-		out.Error(err, "failed to create config")
+		out.Error(err, "Failed to create config")
 		return err
 	}
 
-	out.Success("successfully created config in %s", configpath)
+	out.Success("Configuration saved in %s", configpath)
+  out.Info("\nUse 'cifuzz create' to create your first fuzz test")
 	return
 }
