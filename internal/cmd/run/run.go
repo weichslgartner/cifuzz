2bcdebdfb53657e4063454c25b30895f6101562b
Adrian Dombeck <adrian.dombeck@code-intelligence.de>
2022-06-14 19:34:16 +0200

    Add shell completion for fuzz tests with unknown build systems

--- internal/cmd/run/run.go
+++ internal/cmd/run/run.go
@@ -434,9 +434,39 @@ func validFuzzTests(cmd *cobra.Command, args []string, toComplete string) ([]str
 		return nil, cobra.ShellCompDirectiveError
 	}
 
+	// Read the project config to figure out the build system
+	projectDir, err := config.FindProjectDir()
+	if err != nil {
+		log.Error(err, err.Error())
+		return nil, cobra.ShellCompDirectiveError
+	}
+	conf, err := config.ReadProjectConfig(projectDir)
+	if err != nil {
+		log.Error(err, err.Error())
+		return nil, cobra.ShellCompDirectiveError
+	}
+
+	if conf.BuildSystem == config.BuildSystemCMake {
+		return validCMakeFuzzTests(cmd, args, toComplete)
+	} else if conf.BuildSystem == config.BuildSystemUnknown {
+		// For unknown build systems, the <fuzz test> argument must be
+		// the path to the fuzz test executable, so we use file
+		// completion here (which is only useful if the executable has
+		// been built before, but that's still better than no completion
+		// support)
+		return nil, cobra.ShellCompDirectiveDefault
+	} else {
+		err := errors.Errorf("Unsupported build system \"%s\"", conf.BuildSystem)
+		log.Error(err, err.Error())
+		return nil, cobra.ShellCompDirectiveError
+	}
+}
+
+func validCMakeFuzzTests(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 	matches, err := zglob.Glob(".cifuzz-build/**/.cifuzz/fuzz_tests/*")
 	if err != nil {
-		return nil, cobra.ShellCompDirectiveNoFileComp
+		log.Error(err, err.Error())
+		return nil, cobra.ShellCompDirectiveError
 	}
 	var res []string
 	for _, match := range matches {
