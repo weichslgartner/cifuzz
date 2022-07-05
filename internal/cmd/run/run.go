9c6eeb90952ba67e6aac15288ba3f593f3f9a4de
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-07-05 14:42:35 +0200

    cmd: Extract fuzz test completion into a separate package

--- internal/cmd/run/run.go
+++ internal/cmd/run/run.go
@@ -11,7 +11,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/mattn/go-zglob"
 	"github.com/pkg/errors"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
@@ -21,6 +20,7 @@ import (
 	"code-intelligence.com/cifuzz/internal/build"
 	"code-intelligence.com/cifuzz/internal/build/cmake"
 	"code-intelligence.com/cifuzz/internal/cmd/run/report_handler"
+	"code-intelligence.com/cifuzz/internal/completion"
 	"code-intelligence.com/cifuzz/internal/config"
 	"code-intelligence.com/cifuzz/pkg/cmdutils"
 	"code-intelligence.com/cifuzz/pkg/log"
@@ -92,7 +92,7 @@ func New(config *config.Config) *cobra.Command {
 		//       than just the fallback mode). In particular, explain how a
 		//       "fuzz test" is identified on the CLI.
 		Long:              "",
-		ValidArgsFunction: validFuzzTests,
+		ValidArgsFunction: completion.ValidFuzzTests,
 		Args:              cobra.ExactArgs(1),
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			opts.fuzzTest = args[0]
@@ -456,53 +456,4 @@ func countSeeds(seedDirs []string) (numSeeds uint, err error) {
 		numSeeds += seedsInDir
 	}
 	return numSeeds, nil
-}
-
-func validFuzzTests(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-	// Change the directory if the `--directory` flag was set
-	err := cmdutils.Chdir()
-	if err != nil {
-		log.Error(err, err.Error())
-		return nil, cobra.ShellCompDirectiveError
-	}
-
-	// Read the project config to figure out the build system
-	projectDir, err := config.FindProjectDir()
-	if err != nil {
-		log.Error(err, err.Error())
-		return nil, cobra.ShellCompDirectiveError
-	}
-	conf, err := config.ReadProjectConfig(projectDir)
-	if err != nil {
-		log.Error(err, err.Error())
-		return nil, cobra.ShellCompDirectiveError
-	}
-
-	if conf.BuildSystem == config.BuildSystemCMake {
-		return validCMakeFuzzTests(cmd, args, toComplete)
-	} else if conf.BuildSystem == config.BuildSystemUnknown {
-		// For unknown build systems, the <fuzz test> argument must be
-		// the path to the fuzz test executable, so we use file
-		// completion here (which is only useful if the executable has
-		// been built before, but that's still better than no completion
-		// support)
-		return nil, cobra.ShellCompDirectiveDefault
-	} else {
-		err := errors.Errorf("Unsupported build system \"%s\"", conf.BuildSystem)
-		log.Error(err, err.Error())
-		return nil, cobra.ShellCompDirectiveError
-	}
-}
-
-func validCMakeFuzzTests(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-	matches, err := zglob.Glob(".cifuzz-build/**/.cifuzz/fuzz_tests/*")
-	if err != nil {
-		log.Error(err, err.Error())
-		return nil, cobra.ShellCompDirectiveError
-	}
-	var res []string
-	for _, match := range matches {
-		res = append(res, filepath.Base(match))
-	}
-	return res, cobra.ShellCompDirectiveNoFileComp
 }
---
+++ internal/completion/fuzz_test_completion.go
@@ -0,0 +1,63 @@
+package completion
+
+import (
+	"path/filepath"
+
+	"github.com/mattn/go-zglob"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+
+	"code-intelligence.com/cifuzz/internal/config"
+	"code-intelligence.com/cifuzz/pkg/cmdutils"
+	"code-intelligence.com/cifuzz/pkg/log"
+)
+
+// ValidFuzzTests can be used as a cobra ValidArgsFunction that completes fuzz test names.
+func ValidFuzzTests(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	// Change the directory if the `--directory` flag was set
+	err := cmdutils.Chdir()
+	if err != nil {
+		log.Error(err, err.Error())
+		return nil, cobra.ShellCompDirectiveError
+	}
+
+	// Read the project config to figure out the build system
+	projectDir, err := config.FindProjectDir()
+	if err != nil {
+		log.Error(err, err.Error())
+		return nil, cobra.ShellCompDirectiveError
+	}
+	conf, err := config.ReadProjectConfig(projectDir)
+	if err != nil {
+		log.Error(err, err.Error())
+		return nil, cobra.ShellCompDirectiveError
+	}
+
+	if conf.BuildSystem == config.BuildSystemCMake {
+		return validCMakeFuzzTests(cmd, args, toComplete)
+	} else if conf.BuildSystem == config.BuildSystemUnknown {
+		// For unknown build systems, the <fuzz test> argument must be
+		// the path to the fuzz test executable, so we use file
+		// completion here (which is only useful if the executable has
+		// been built before, but that's still better than no completion
+		// support)
+		return nil, cobra.ShellCompDirectiveDefault
+	} else {
+		err := errors.Errorf("Unsupported build system \"%s\"", conf.BuildSystem)
+		log.Error(err, err.Error())
+		return nil, cobra.ShellCompDirectiveError
+	}
+}
+
+func validCMakeFuzzTests(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	matches, err := zglob.Glob(".cifuzz-build/**/.cifuzz/fuzz_tests/*")
+	if err != nil {
+		log.Error(err, err.Error())
+		return nil, cobra.ShellCompDirectiveError
+	}
+	var res []string
+	for _, match := range matches {
+		res = append(res, filepath.Base(match))
+	}
+	return res, cobra.ShellCompDirectiveNoFileComp
+}
