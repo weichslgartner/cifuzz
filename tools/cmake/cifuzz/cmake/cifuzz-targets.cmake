9d3ecb096d4f2cc80c363c375e0d7bb88042baaa
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-07-13 13:38:58 +0200

    cmake: Fix for projects that don't enable the C language
    Previously, projects with `project(MyProject LANGUAGES CXX)` in their
    top-level CMakeLists.txt would fail to build the replayer.
    
    This is fixed by marking replayer.c as a C++ source file if C is not
    in ENABLED_LANGUAGES.

--- tools/cmake/cifuzz/cmake/cifuzz-targets.cmake
+++ tools/cmake/cifuzz/cmake/cifuzz-targets.cmake
@@ -1,5 +1,12 @@
 add_library(cifuzz_internal_replayer STATIC EXCLUDE_FROM_ALL
             "${CMAKE_CURRENT_LIST_DIR}/../src/replayer.c")
+# If a CXX-only project depends on the replayer, it won't be able to compile and link the replayer's C source file.
+# Using enable_language(C) from a package is discouraged, but we can work around this by marking the replayer as a CXX
+# target if C is not enabled - it should build just fine with any C++ compiler.
+# https://discourse.cmake.org/t/is-it-appropriate-to-use-enable-language-in-a-cmake-package-file/4335/2
+if(NOT C IN_LIST ENABLED_LANGUAGES)
+  set_target_properties(cifuzz_internal_replayer PROPERTIES LINKER_LANGUAGE CXX)
+endif()
 if(CIFUZZ_SANITIZERS)
   target_compile_definitions(cifuzz_internal_replayer PRIVATE CIFUZZ_HAS_SANITIZER)
 endif()
