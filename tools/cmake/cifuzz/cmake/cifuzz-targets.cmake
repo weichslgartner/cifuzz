7755572a67d8f64d7a583d0d95e81a7727ae4597
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-07-14 18:31:38 +0200

    cmake: Ensure that replayer is compiled into CXX-only projects
    The fix in 9d3ecb096d4f2cc80c363c375e0d7bb88042baaa was incomplete as it
    only modified the linker language, not the source file language. As a
    result, in CXX-only projects, the replayer static library would be empty.
    
    This is fixed by marking the source file as C++ so that CMake can use
    the configured C++ compiler, but passing in flags that make the C++
    compiler treat the file as C. Just in case, the previous commit ensures
    that it compiles just fine treated as C++.

--- tools/cmake/cifuzz/cmake/cifuzz-targets.cmake
+++ tools/cmake/cifuzz/cmake/cifuzz-targets.cmake
@@ -5,10 +5,20 @@ if(MSVC)
   target_compile_options(cifuzz_internal_replayer PUBLIC /MTd)
 endif()
 # If a CXX-only project depends on the replayer, it won't be able to compile and link the replayer's C source file.
-# Using enable_language(C) from a package is discouraged, but we can work around this by marking the replayer as a CXX
-# target if C is not enabled - it should build just fine with any C++ compiler.
+# Using enable_language(C) from a package is discouraged, but we can work around this by asking the C++ compiler to
+# compile the replayer as a C target if C is not enabled.
 # https://discourse.cmake.org/t/is-it-appropriate-to-use-enable-language-in-a-cmake-package-file/4335/2
 if(NOT C IN_LIST ENABLED_LANGUAGES)
+  set_property(SOURCE "${CMAKE_CURRENT_LIST_DIR}/../src/replayer.c"
+               APPEND
+               PROPERTY LANGUAGE CXX)
+  # Make the C++ compiler compile replayer.c as a C file without a warning such as:
+  # clang: warning: treating 'c' input as 'c++' when in C++ mode, this behavior is deprecated
+  if(MSVC)
+    target_compile_options(cifuzz_internal_replayer PRIVATE /TC)
+  else()
+    target_compile_options(cifuzz_internal_replayer PRIVATE -x c)
+  endif()
   set_target_properties(cifuzz_internal_replayer PROPERTIES LINKER_LANGUAGE CXX)
 endif()
 if(CIFUZZ_SANITIZERS)
