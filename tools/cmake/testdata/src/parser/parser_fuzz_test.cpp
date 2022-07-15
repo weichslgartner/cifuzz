a626b8e433095ecba2c18c6d1d52f732d5ecdeba
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-07-15 12:58:05 +0200

    cmake: Test that FuzzedDataProvider can be used

--- tools/cmake/testdata/src/parser/parser_fuzz_test.cpp
+++ tools/cmake/testdata/src/parser/parser_fuzz_test.cpp
@@ -2,11 +2,13 @@
 
 #include "parser.h"
 #include <cifuzz/cifuzz.h>
+#include <fuzzer/FuzzedDataProvider.h>
 
 FUZZ_TEST_SETUP() {
   std::cout << "FUZZ_TEST_SETUP ran" << std::endl;
 }
 
 FUZZ_TEST(const std::uint8_t *data, std::size_t size) {
-  parse(std::string(reinterpret_cast<const char*>(data), size));
+  FuzzedDataProvider fdp(data, size);
+  parse(fdp.ConsumeRemainingBytesAsString());
 }
