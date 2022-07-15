28bd3deb1cc940147b3335c0424a3ed3065a2cb2
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-07-15 16:59:07 +0200

    cmake: Use enable_language(C) on Windows
    Trying the same trick as with clang/gcc leads to linker errors such as:
    
    ```
    cifuzz_internal_replayer.lib(replayer.obj) : error LNK2019: unresolved external symbol "int __cdecl LLVMFuzzerInitialize(int *,char * * *)" (?LLVMFuzzerInitialize@@YAHPEAHPEAPEAPEAD@Z) referenced in function main [C:\Users\runneradmin\AppData\Local\Temp\cifuzz cmake2382850796\build3898718806\src\parser\parser_fuzz_test.vcxproj]
    cifuzz_internal_replayer.lib(replayer.obj) : error LNK2019: unresolved external symbol "char const * __cdecl cifuzz_test_name(void)" (?cifuzz_test_name@@YAPEBDXZ) referenced in function "void __cdecl print_summary(char const *)" (?print_summary@@YAXPEBD@Z) [C:\Users\runneradmin\AppData\Local\Temp\cifuzz cmake2382850796\build3898718806\src\parser\parser_fuzz_test.vcxproj]
    cifuzz_internal_replayer.lib(replayer.obj) : error LNK2019: unresolved external symbol "char const * __cdecl cifuzz_seed_corpus(void)" (?cifuzz_seed_corpus@@YAPEBDXZ) referenced in function main [C:\Users\runneradmin\AppData\Local\Temp\cifuzz cmake2382850796\build3898718806\src\parser\parser_fuzz_test.vcxproj]
    ```
    
    I haven't been able to fix them, so we are now trying something else.

--- tools/cmake/cifuzz/cmake/cifuzz-targets.cmake
+++ tools/cmake/cifuzz/cmake/cifuzz-targets.cmake
@@ -9,17 +9,20 @@ endif()
 # compile the replayer as a C target if C is not enabled.
 # https://discourse.cmake.org/t/is-it-appropriate-to-use-enable-language-in-a-cmake-package-file/4335/2
 if(NOT C IN_LIST ENABLED_LANGUAGES)
-  set_property(SOURCE "${CMAKE_CURRENT_LIST_DIR}/../src/replayer.c"
-               APPEND
-               PROPERTY LANGUAGE CXX)
-  # Make the C++ compiler compile replayer.c as a C file without a warning such as:
-  # clang: warning: treating 'c' input as 'c++' when in C++ mode, this behavior is deprecated
   if(MSVC)
-    target_compile_options(cifuzz_internal_replayer PRIVATE /TC)
+    # TODO(fmeum): For some reason, setting /TC for MSVC doesn't make it into the compiler invocation, so we can't apply
+    #  the same trick as for clang/gcc below. Go with an enable_language call instead, which should work if we are
+    #  included from the top-level CMakeLists.txt as we suggest in our init command.
+    enable_language(C)
   else()
+    set_property(SOURCE "${CMAKE_CURRENT_LIST_DIR}/../src/replayer.c"
+                 APPEND
+                 PROPERTY LANGUAGE CXX)
+    # Make the C++ compiler compile replayer.c as a C file without a warning such as:
+    # clang: warning: treating 'c' input as 'c++' when in C++ mode, this behavior is deprecated
     target_compile_options(cifuzz_internal_replayer PRIVATE -x c)
+    set_target_properties(cifuzz_internal_replayer PROPERTIES LINKER_LANGUAGE CXX)
   endif()
-  set_target_properties(cifuzz_internal_replayer PROPERTIES LINKER_LANGUAGE CXX)
 endif()
 if(CIFUZZ_SANITIZERS)
   target_compile_definitions(cifuzz_internal_replayer PRIVATE CIFUZZ_HAS_SANITIZER)
