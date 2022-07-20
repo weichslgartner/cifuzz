a53ae623f9d07a2969e057ef039136814e9a5b21
Adrian Dombeck <adrian.dombeck@code-intelligence.de>
2022-07-20 17:30:16 +0200

    init: Fix filename of cifuzz.yaml in usage string

--- internal/cmd/init/init.go
+++ internal/cmd/init/init.go
@@ -20,7 +20,7 @@ func New() *cobra.Command {
 		Use:   "init",
 		Short: "Set up a project for use with cifuzz",
 		Long: "This command sets up a project for use with cifuzz, creating a " +
-			"`.cifuzz.yaml` config file.",
+			"`cifuzz.yaml` config file.",
 		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return run(cmd, args, opts)
