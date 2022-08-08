67a03a0290059533efd56329f712ca4f134a8170
Jochen <jochen.hilgers@gmail.com>
2022-08-08 14:56:07 +0200

    fix(install): make install.sh compatible to "original" sh
    ... to increase compatibility
    
    Refs: CLI-338

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
+set -e
+
+BASE_URL="https://github.com/CodeIntelligenceTesting/cifuzz/releases/latest/download/"
 
 # detect os to get the correct url for the installer
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  DOWNLOAD_URL="https://github.com/CodeIntelligenceTesting/cifuzz/releases/latest/download/cifuzz_installer_linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  if [[ $(uname -m) == 'arm64' ]]; then
-    DOWNLOAD_URL="https://github.com/CodeIntelligenceTesting/cifuzz/releases/latest/download/cifuzz_installer_darwin_arm64"
-  else
-    DOWNLOAD_URL="https://github.com/CodeIntelligenceTesting/cifuzz/releases/latest/download/cifuzz_installer_darwin"
-  fi
-elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
-  DOWNLOAD_URL="https://github.com/CodeIntelligenceTesting/cifuzz/releases/latest/download/cifuzz_installer_windows"
-else
-  echo "No installer available for this operating system '$OSTYPE'"
-  exit 1
-fi
+case $(uname -s) in 
+  Linux*)
+    INSTALLER="cifuzz_installer_linux" ;;
 
+  Windows*|MINGW*|MSYS*|CYGWIN*)  
+    INSTALLER="cifuzz_installer_windows.exe" ;;
+
+  Darwin*)
+    if [ $(uname -m) = 'arm64' ]; then
+      INSTALLER="cifuzz_installer_darwin_arm64"
+    else
+      INSTALLER="cifuzz_installer_darwin"
+    fi ;;
+
+  *) 
+    echo "No installer available for this operating system '$(uname -s) $(uname -m)'"
+    exit 1 ;;
+esac
+
+DOWNLOAD_URL=$BASE_URL$INSTALLER
 echo -n "Downloading installer... "
 curl -fsSL -o cifuzz_installer $DOWNLOAD_URL
 echo "ok"
