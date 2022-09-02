743476f09eed72317dd9bfd034ccb91e6982de0c
Adrian Dombeck <adrian.dombeck@code-intelligence.com>
2022-09-02 12:37:49 +0200

    executil: Document that TerminateProcessGroup causes Wait to block
    ... when used together with StdoutTeePipe or StderrTeePipe.

--- util/executil/executil_windows.go
+++ util/executil/executil_windows.go
@@ -8,6 +8,15 @@ import (
 	"github.com/pkg/errors"
 )
 
+// TerminateProcessGroup uses the taskkill command with the /t and /f
+// parameters to forcefully terminate the process of the command and
+// any child processes started by it.
+// See https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/taskkill
+//
+// Important: Note that on Windows, when using StdoutTeePipe or
+// StderrTeePipe, TerminateProcessGroup doesn't cause the process
+// to exit if the pipes were not closed yet. In that case, Wait will
+// block until the pipes are closed.
 func (c *Cmd) TerminateProcessGroup() error {
 	// Based on https://stackoverflow.com/a/44551450/2804197
 	// Original author: https://stackoverflow.com/users/301049/rots
