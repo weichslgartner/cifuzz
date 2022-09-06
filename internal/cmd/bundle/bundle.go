a08f8f5d4a4ee57cfea6ea0ded01e5f4be3f1c48
Adrian Dombeck <adrian.dombeck@code-intelligence.com>
2022-09-06 15:41:48 +0200

    bundle: Add command-line flags

--- internal/cmd/bundle/bundle.go
+++ internal/cmd/bundle/bundle.go
@@ -93,6 +93,11 @@ If no fuzz tests are specified all fuzz tests are added to the bundle.`,
 			// function, because that would re-bind viper keys which
 			// were bound to the flags of other commands before.
 			cmdutils.ViperMustBindPFlag("build-jobs", cmd.Flags().Lookup("build-jobs"))
+			cmdutils.ViperMustBindPFlag("seed-corpus-dirs", cmd.Flags().Lookup("seed-corpus"))
+			cmdutils.ViperMustBindPFlag("dict", cmd.Flags().Lookup("dict"))
+			cmdutils.ViperMustBindPFlag("engine-args", cmd.Flags().Lookup("engine-arg"))
+			cmdutils.ViperMustBindPFlag("fuzz-test-args", cmd.Flags().Lookup("fuzz-test-arg"))
+			cmdutils.ViperMustBindPFlag("timeout", cmd.Flags().Lookup("timeout"))
 
 			_, err := config.ParseProjectConfig(opts)
 			if err != nil {
@@ -118,6 +123,14 @@ If no fuzz tests are specified all fuzz tests are added to the bundle.`,
 
 	cmd.Flags().Uint("build-jobs", 0, "Maximum number of concurrent processes to use when building.\nIf argument is omitted the native build tool's default number is used.\nOnly available when the build system is CMake.")
 	cmd.Flags().Lookup("build-jobs").NoOptDefVal = "0"
+	// TODO(afl): Also link to https://aflplus.plus/docs/fuzzing_in_depth/#a-collecting-inputs
+	cmd.Flags().StringArrayP("seed-corpus", "s", nil, "A `directory` containing sample inputs for the code under test.\nSee https://llvm.org/docs/LibFuzzer.html#corpus.")
+	// TODO(afl): Also link to https://github.com/AFLplusplus/AFLplusplus/blob/stable/dictionaries/README.md
+	cmd.Flags().String("dict", "", "A `file` containing input language keywords or other interesting byte sequences.\nSee https://llvm.org/docs/LibFuzzer.html#dictionaries.")
+	// TODO(afl): Also link to https://www.mankier.com/8/afl-fuzz
+	cmd.Flags().StringArray("engine-arg", nil, "Command-line `argument` to pass to the fuzzing engine.\nSee https://llvm.org/docs/LibFuzzer.html#options.")
+	cmd.Flags().StringArray("fuzz-test-arg", nil, "Command-line `argument` to pass to the fuzz test.")
+	cmd.Flags().Duration("timeout", 0, "Maximum time to run the fuzz test, e.g. \"30m\", \"1h\". The default is to run indefinitely.")
 	cmd.Flags().StringVarP(&opts.outputPath, "output", "o", "", "Output path of the artifact (.tar.gz)")
 
 	return cmd
