4e1bd086020a58356e5c19219044d54d7b78bea2
mlsandner <maren.sandner@code-intelligence.com>
2022-10-14 15:59:18 +0200

    completion: Support fuzz test completion with bazel
    Co-authored-by: Adrian Dombeck <adrian.dombeck@code-intelligence.com>

--- internal/completion/fuzz_test_completion.go
+++ internal/completion/fuzz_test_completion.go
@@ -1,6 +1,7 @@
 package completion
 
 import (
+	"bufio"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -14,7 +15,12 @@ import (
 	"code-intelligence.com/cifuzz/internal/cmdutils"
 	"code-intelligence.com/cifuzz/internal/config"
 	"code-intelligence.com/cifuzz/pkg/log"
+	"code-intelligence.com/cifuzz/util/regexutil"
 )
+
+// This regex is based on the bazel bash completion script, see:
+// https://github.com/bazelbuild/bazel/blob/021c2a053780d697899cbcbd76a032c72cd5cbbb/scripts/bazel-complete-template.bash#L173
+var bazelFuzzTestTargetPattern = regexp.MustCompile(`cc_fuzz_test *\([^)]* {0,1}name *= *['"](?P<name>[a-zA-Z0-9_.+=,@~-]*)['"][^)]*\)`)
 
 // ValidFuzzTests can be used as a cobra ValidArgsFunction that completes fuzz test names.
 func ValidFuzzTests(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
@@ -37,6 +43,8 @@ func ValidFuzzTests(cmd *cobra.Command, args []string, toComplete string) ([]str
 	}
 
 	switch conf.BuildSystem {
+	case config.BuildSystemBazel:
+		return validBazelFuzzTests(toComplete)
 	case config.BuildSystemCMake:
 		return validCMakeFuzzTests(conf.ProjectDir)
 	case config.BuildSystemMaven, config.BuildSystemGradle:
@@ -53,6 +61,97 @@ func ValidFuzzTests(cmd *cobra.Command, args []string, toComplete string) ([]str
 		log.Error(err, err.Error())
 		return nil, cobra.ShellCompDirectiveError
 	}
+}
+
+func validBazelFuzzTests(toComplete string) ([]string, cobra.ShellCompDirective) {
+	workDir, err := os.Getwd()
+	if err != nil {
+		log.Error(err)
+		return nil, cobra.ShellCompDirectiveError
+	}
+
+	var buildFiles []string
+	err = filepath.WalkDir(workDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(workDir, path)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		// Don't skip the current working directory
+		if relPath == "." {
+			return nil
+		}
+
+		if d.IsDir() {
+			// Skip walking the directory if it doesn't start with the
+			// toComplete string
+			if !strings.HasPrefix(relPath, toComplete) {
+				return fs.SkipDir
+			}
+			return nil
+		}
+
+		baseName := filepath.Base(relPath)
+		if baseName == "BUILD.bazel" || baseName == "BUILD" {
+			buildFiles = append(buildFiles, relPath)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Error(err)
+		return nil, cobra.ShellCompDirectiveError
+	}
+
+	var res []string
+	for _, buildFile := range buildFiles {
+		file, err := os.Open(buildFile)
+		if err != nil {
+			// Command completion is best-effort: Do not fail on errors
+			log.Error(err)
+			continue
+		}
+
+		// Read build file and remove comments and newlines, which is
+		// the same the bazel bash completion script does, see:
+		// https://github.com/bazelbuild/bazel/blob/021c2a053780d697899cbcbd76a032c72cd5cbbb/scripts/bazel-complete-template.bash#L166-L167
+		var text string
+		sc := bufio.NewScanner(file)
+		for sc.Scan() {
+			line := strings.TrimSpace(sc.Text())
+			if !strings.HasPrefix(line, "#") {
+				text += " " + line
+			}
+		}
+
+		if !strings.Contains(text, "cc_fuzz_test") {
+			continue
+		}
+
+		targetNames, found := regexutil.FindNamedGroupsMatch(bazelFuzzTestTargetPattern, text)
+		if !found {
+			continue
+		}
+
+		for _, name := range targetNames {
+			// Construct the relative target label (that's the term used
+			// by bazel for the target identifier, see
+			// https://bazel.build/concepts/labels)
+			var relLabel string
+			relPackageName := filepath.Dir(buildFile)
+			if relPackageName == "." {
+				relLabel = name
+			} else {
+				relLabel = relPackageName + ":" + name
+			}
+			res = append(res, relLabel)
+		}
+	}
+
+	return res, cobra.ShellCompDirectiveNoFileComp
 }
 
 func validCMakeFuzzTests(projectDir string) ([]string, cobra.ShellCompDirective) {
