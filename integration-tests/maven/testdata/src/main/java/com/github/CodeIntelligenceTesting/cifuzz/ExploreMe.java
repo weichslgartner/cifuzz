5f8293ab28d71a0c4a2fee02382e203c066463dc
Florian Gläßer <florian.glaesser@code-intelligence.com>
2022-10-24 17:35:14 +0200

    integration-tests: Remove print statements

--- integration-tests/maven/testdata/src/main/java/com/github/CodeIntelligenceTesting/cifuzz/ExploreMe.java
+++ integration-tests/maven/testdata/src/main/java/com/github/CodeIntelligenceTesting/cifuzz/ExploreMe.java
@@ -3,20 +3,11 @@ package com.github.CodeIntelligenceTesting.cifuzz;
 public class ExploreMe {
     // Function with multiple paths that can be discovered by a fuzzer.
     public static void exploreMe(int a, int b, String c) {
-        System.out.printf("a: %d, b: %d, c: %s\n", a, b, c);
-
         if (a >= 20000) {
-            System.out.println("branch 1");
-
             if (b >= 2000000) {
-                System.out.println("branch 2");
-
                 if (b - a < 100000) {
-                    System.out.println("branch 3");
-
                     // Create reflective call
                     if (c.startsWith("@")) {
-                        System.out.println("branch 4");
                         String className = c.substring(1);
                         try {
                             Class.forName(className);
@@ -25,9 +16,6 @@ public class ExploreMe {
                     }
                 }
             }
-        } else {
-            System.out.println("this is the default path");
         }
-        System.out.println("---------");
     }
 }
